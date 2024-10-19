dfa9020258492601cfcd9953b7a5cae76b341430
Iliya <facyoucraft@gmail.com>
2024-10-20 01:31:43 +0300

    feat(project): remove next.js app for change structure

--- src/app/components/randomMessages.js
+++
@@ -1,9 +0,0 @@
-function getRandomMessage(messages) {
-    if (!messages || messages.length === 0) {
-        return null;
-    }
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    return messages[randomIndex];
-}
-
-module.exports = { getRandomMessage };
--- src/app/components/voiceHandler.js
+++
@@ -1,9 +0,0 @@
-const { getRandomMessage } = require('/src/app/components/randomMessages');
-
-module.exports = async function handleVoiceMessage(ctx) {
-    const mes = []; // Заполни массив своими фразами
-    const randomReply = getRandomMessage(mes);
-    if (randomReply) {
-        ctx.reply(randomReply);
-    }
-};
