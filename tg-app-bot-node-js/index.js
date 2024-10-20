0eac61fe34e09102a0149027285b287a7c2ee496
Iliya <facyoucraft@gmail.com>
2024-10-20 18:29:09 +0300

    feat(app): init tg bot and add logic

--- tg-app-bot-node-js/index.js
+++ tg-app-bot-node-js/index.js
@@ -1,20 +1,39 @@
-const TelegramBot = require("node-telegram-bot-api");
-// require("dotenv").config();
+import TelegramBot from "node-telegram-bot-api";
+import dotenv from "dotenv";
+import { handleVoice } from "./components/shared/voiceHandler.js";
+import { handleReplies } from "./components/shared/replyHandler.js";
+import { handleGender } from "./components/shared/genderHandler.js";
+import { handleTop } from "./components/shared/topHandler.js";
 
-const API_TOKEN = "5701346051:AAGc8mZdfPpB3HtYczSMBmVad070TBWJkYs";
+dotenv.config();
+
+const API_TOKEN = process.env.API_TOKEN;
 const bot = new TelegramBot(API_TOKEN, { polling: true });
 
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Welcome to the bot!");
+bot.on("polling_error", (error) => {
+  console.error("Polling error:", error);
 });
 
-// bot.on("message", async (msg) => {
-//   if (msg.voice) {
-//     await handleVoice(bot, msg);
-//   } else if (msg.reply_to_message) {
-//     await handleReplies(bot, msg);
-//   } else {
-//     await handleCommands(bot, msg);
-//   }
+// bot.on("message", (msg) => {
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, "Welcome to the bot!");
 // });
+
+bot.on("message", async (msg) => {
+  if (msg.voice) {
+    await handleVoice(bot, msg);
+  } else if (msg.reply_to_message) {
+    await handleReplies(bot, msg);
+  }
+  // } else {
+  //   await handleCommands(bot, msg);
+  // }
+});
+
+bot.onText(/\/pidor/, async (msg) => {
+  await handleGender(bot, msg);
+});
+
+bot.onText(/\/top/, async (msg) => {
+  await handleTop(bot, msg);
+});
